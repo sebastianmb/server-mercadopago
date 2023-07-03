5b31b5da231fdae601fbbc3f912fe9020084f0db
sebastianmb <sebastianmb88@gmail.com>
2023-07-02 23:49:33 -0500

    Creacion back pasarela de pago

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mercadopago = require("mercadopago");
+
+app.use(express.json());
+app.use(cors());
+
+//min 16
