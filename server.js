af7b298e6b992dcfa206078fdfa77ddf7b404501
sebastianmb <sebastianmb88@gmail.com>
2023-07-03 22:07:51 -0500

    Se crea el server mercado pago

--- server.js
+++ server.js
@@ -6,4 +6,37 @@ const mercadopago = require("mercadopago");
 app.use(express.json());
 app.use(cors());
 
-//min 16
+app.get('/',function(req,res){
+    res.send("El servidor de mercado pago funciona! ")
+});
+app.post("/create_preference", (req, res) => {
+
+	let preference = {
+		items: [
+			{
+				title: req.body.description,
+				unit_price: Number(req.body.price),
+				quantity: Number(req.body.quantity),
+			}
+		],
+		back_urls: { // ajustar url de retorno
+			"success": "http://localhost:8080/feedback",
+			"failure": "http://localhost:8080/feedback",
+			"pending": "http://localhost:8080/feedback"
+		},
+		auto_return: "approved",
+	};
+
+	mercadopago.preferences.create(preference)
+		.then(function (response) {
+			res.json({
+				id: response.body.id
+			});
+		}).catch(function (error) {
+			console.log(error);
+		});
+});
+
+app.listen(8080, () => {
+	console.log("The server is now running on Port 8080");
+});
