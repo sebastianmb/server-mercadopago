52f2fc2445c9d63ca8303b31152f07ce1a959527
sebastianmb <sebastianmb88@gmail.com>
2023-07-06 23:04:33 -0500

    Ajustes backend pago pse

--- server.js
+++ server.js
@@ -6,8 +6,10 @@ const mercadopago = require("mercadopago");
 app.use(express.json());
 app.use(cors());
 
+
+/* Metodo checkpro
 mercadopago.configure({
-	access_token:"TEST-1226444629337365-070415-300051315d51f196f21dd572c0bba03f-329144871",
+	access_token:"",
 });
 
 app.get('/',function(req,res){
@@ -41,6 +43,68 @@ app.post("/create_preference", (req, res) => {
 		});
 });
 
+// Ruta para obtener los métodos de pago
+app.get("/payment_methods", (req, res) => {
+	mercadopago.paymentMethods.all()
+	  .then(response => {
+		res.json(response);
+	  })
+	  .catch(error => {
+		res.status(500).json({ error: "Error al obtener los métodos de pago" });
+	  });
+  });
+
+
 app.listen(8080, () => {
+	console.log("The server is now running on Port 8080");
+});
+
+*/
+
+mercadopago.configurations.setAccessToken('TU_ACCESS_TOKEN');
+
+app.post('/procesar-pago', (req, res) => {
+	const payment = req.body;
+  
+	const paymentData = {
+	  transaction_amount: 5000,
+	  description: 'Título del producto',
+	  payment_method_id: 'pse',
+	  payer: {
+		entity_type: 'individual',
+		email: payment.email,
+		identification: {
+		  type: payment.identificationType,
+		  number: payment.identificationNumber
+		}
+	  },
+	  additional_info: {
+		ip_address: '127.0.0.1'
+	  },
+	  transaction_details: {
+		financial_institution: '1009'
+	  },
+	  callback_url: 'http://www.your-site.com'
+	};
+  
+	mercadopago.payment.save(paymentData)
+	  .then(function(response) {
+		res.status(response.status).json({
+		  status: response.body.status,
+		  status_detail: response.body.status_detail,
+		  id: response.body.id,
+		});
+	  })
+	  .catch(function(error) {
+		res.status(error.status).send(error);
+	  });
+  });
+  
+
+  app.get('/',function(req,res){
+    res.send("El servidor de mercado pago funciona! ")
+});
+
+  app.listen(8080, () => {
 	console.log("The server is now running on Port 8080");
 });
