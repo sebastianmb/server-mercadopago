505306b1581fb883d79e26e97b398f28709bdd65
sebastianmb <sebastianmb88@gmail.com>
2023-07-13 23:30:11 -0500

    ajustes backend para que reciba el pago

--- server.js
+++ server.js
@@ -1,26 +1,51 @@
+const express = require('express');
+const bodyParser = require('body-parser');
 const mercadopago = require("mercadopago");
+const cors= require('cors');
+
 mercadopago.configure({
   access_token: "TEST-5926084439420650-071014-ec73e925d2124822c5e53a2723120c5a-329144871",
 });
 
-// Crear preferencia en Mercado Pago
-const preference = {
-	items: [
-	  {
-		id: "item-ID-1234",
-		title: "Mi producto",
-		quantity: 1,
-		unit_price: 75,
-	  },
-	],
+
+
+const app = express();
+
+app.use(cors()); // Habilitar CORS
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Endpoint /process_payment
+app.post('/process_payment', (req, res) => {
+  const token = req.body.token;
+  // Obtener otros datos necesarios para el pago
+
+  // Crear el objeto de preferencia de pago
+  const preference = {
+    items: [
+      {
+        id: "item-ID-1234",
+        title: "Mi producto",
+        quantity: 1,
+        unit_price: 75,
+      },
+    ],
   };
-  
+
+  // Crear la preferencia de pago en Mercado Pago
   mercadopago.preferences.create(preference)
-	.then((response) => {
-	  const preferenceId = response.body.id;
-	  console.log("ID de preferencia:", preferenceId);
-	  // Guardar o enviar el preferenceId como sea necesario
-	})
-	.catch((error) => {
-	  console.error("Error al crear la preferencia:", error);
-	});
+    .then(response => {
+      const preferenceId = response.body.id;
+      console.log("ID de preferencia:", preferenceId);
+
+      // Aquí puedes procesar la respuesta de Mercado Pago y realizar acciones adicionales
+      res.send('Pago exitoso'); // O cualquier otra respuesta que desees enviar al cliente
+    })
+    .catch(error => {
+      res.send('Error en el pago: ' + error); // O manejar el error de otra manera
+    });
+});
+
+// Ejemplo de uso
+app.listen(8080, () => {
+  console.log('Servidor escuchando en el puerto 8080');
+});
